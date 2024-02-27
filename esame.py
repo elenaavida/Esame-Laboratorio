64096b419efc37079c752e339286989c508e3986
elenaavida <150357687+elenaavida@users.noreply.github.com>
2024-02-27 16:04:29 +0000

    mod

--- esame.py
+++ esame.py
@@ -2,7 +2,18 @@
 class ExamException(Exception):
     pass
 
-class CSVTimeSeriesFile:
+class CSVFile:
+    
+    def __init__(self, name):
+        self.name = name
+        self.can_read = True
+        try:
+            file = open(self.name, 'r')
+            file.readline()
+        except Exception as e:
+            self.can_read = False
+        
+class CSVTimeSeriesFile(CSVFile):
     #inizializzatore che prende il nome del file in input e lo salva nell'attributo
     def __init__(self, name):
         self.name = name
@@ -11,10 +22,11 @@ class CSVTimeSeriesFile:
         #definisco la lista di liste che verrà ritornata, il primo elemento della lista annidata è la data e il secondo il numero di passeggeri
         time_series = []
         
-        return
+        return time_series
 
 #funzione che deve ritornare il dizionario di dizionari con l'anno come chiave del dizionario esterno e come valore un altro dizionario che avrà come 
 #due chiavi min e max che avranno due valori ovvero le liste che conterranno i mesi con i passeggeri minimi e massimi
 def find_min_max(time_series):
     dict_min_max = {'min': [], 'max': []}
-    return 
+    
+    return dict_min_max
